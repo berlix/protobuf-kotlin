ad0861e5c37b8de1449762ebec278e01d1499945
Felix Engelhardt <felix.engelhardt@eidu.com>
2023-08-01 21:49:34 +0200

    🎨 ListEncoder: remove unused import

--- serialization/src/commonMain/kotlin/pro/felixo/proto3/encoding/ListEncoder.kt
+++ serialization/src/commonMain/kotlin/pro/felixo/proto3/encoding/ListEncoder.kt
@@ -3,7 +3,6 @@ package pro.felixo.proto3.encoding
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerializationStrategy
 import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.CompositeEncoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.modules.SerializersModule
 import pro.felixo.proto3.FieldNumber
